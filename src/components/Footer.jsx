49caabcdfc84d23bf219657ad896baf468cd6fbb
AlexandrZeroff <zeroffalexandr@gmail.com>
2023-03-10 13:38:11 +0200

    Footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,6 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row md:justify-between p-default my-8">
         <p className="text-base">© Copyright 2022</p>
         <div className="mt-4 md:mt-0">
-          <a href="tel:+380985525943">+380 98 552 5943</a>
-          <a href="mailto:partnership@inseed.marketing" className="block mt-2">
-            partnership@inseed.marketing
-          </a>
           <p className="text-base mt-2">
             1089 Budapest, Korányi Sándor utca 4. 4. Ajtó, Hungary
           </p>
